97ce95619c02e2a3fd71466347a130b01415d5f1
Su_Pu#1A4472 <shown22327989@gmail.com>
2023-05-11 14:38:50 +0900

    variable's mutable, immutable, shadowing,

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,32 @@
 fn main() {
+    //下記の例ではx=6の部分でxの値が変更できない旨のコンパイルエラーが発生する
+    /*
     let x = 5;
     println!("The value of x is: {}", x);   // xの値は{}です
     x = 6;
     println!("The value of x is: {}", x);
+    */
+
+    //mutキーワードを追加してxの値を可変(mutable)にする(エラーは発生しない)
+    /*
+    let mut x = 5;
+    println!("The value of x is: {}", x);   //xの値は{}です
+    x = 6;
+    println!("The value of x is: {}", x);   //xの値は{}です
+    */
+
+    //シャドーイングしてみる
+    let x = 5;
+    let x = x + 1;
+    {
+        let x = x * 2;
+        println!("The value of x in the inner scope is: {}", x);
+        //->12が出力される
+    }
+    println!("The value of x is: {}", x);
+    //->6が出力される
+
+    // //spacetest
+    // let mut spaces = "    ";
+    // spaces = spaces.len();
 }
